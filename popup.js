c3656488b5ea9cd70d9e54cc9d1b35f30b11d745
shinamon <76248816+shinamon610@users.noreply.github.com>
2022-08-19 23:28:08 +0900

    refactor to prevent read html value

--- popup.js
+++ popup.js
@@ -1,19 +1,26 @@
 const inputQ = getElById("inp")
 const wrapper = getElsByClass("wrapper")[0]
 
-// data for test
-const testlist = [
-    { id: 0, summary: "ITdes", list: ["aaaa", "bbbb"], key: "e" },
-    { id: 1, summary: "ITdd", list: ["cc", "dd"], key: "e" }
-]
 
-
+function getDataAndDo(func) {
+    chrome.storage.sync.get(["detailsData"], (data) => {
+        if (data.detailsData == null) {
+            func([])
+        } else {
+            func(data.detailsData)
+        }
+    })
+}
 
 window.onload = () => {
-    chrome.storage.sync.get(["detailsData"], (data) => {
-        makeLines(data.detailsData)
-    });
+    getDataAndDo(showData)
 }
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (changes.detailsData) {
+        showData(changes.detailsData.newValue)
+    }
+})
 
 function makeId(data) {
     maxId = 0
@@ -23,10 +30,10 @@ function makeId(data) {
     return maxId + 1
 }
 
-function addSummary(data, summary) {
-    data.push({ id: makeId(data), summary: summary, list: [], key: "e" })
-    chrome.storage.sync.set({ detailsData: data }, () => {
-        makeLines(data)
+function addSummary(summary) {
+    getDataAndDo((details) => {
+        details.push({ id: makeId(details), summary: summary, urls: [], key: "e" })
+        showData(details)
     })
 }
 
@@ -41,18 +48,23 @@ function makeDetailsId(id) {
 function makeButtonId(id) {
     return "btn" + id
 }
+function makeSummaryId(id) {
+    return "sum" + id
+}
 
-
+// use in showData
 function makeLine(lineData) {
+    const ID = lineData["id"]
     const details = document.createElement("details")
-    const idName = makeDetailsId(lineData["id"])
+    const idName = makeDetailsId(ID)
     details.id = idName
     details.tabIndex = "-1"
     const summary = document.createElement("summary")
     summary.textContent = lineData["summary"]
+    summary.id = makeSummaryId(ID)
     details.appendChild(summary)
 
-    lineData["list"].forEach(url => {
+    lineData["urls"].forEach(url => {
         const li = document.createElement("li")
         const input = document.createElement("input")
         input.value = url
@@ -76,25 +88,46 @@ function makeLine(lineData) {
     wrapper.appendChild(to)
 
     const searchButton = document.createElement("button")
-    searchButton.id = makeButtonId(lineData["id"])
+    searchButton.id = makeButtonId(ID)
     const span = document.createElement("span")
     span.className = "material-symbols-outlined"
     span.style = "font-size: 15px;"
     span.textContent = "search"
     searchButton.appendChild(span)
     wrapper.appendChild(searchButton)
+
+    searchButton.addEventListener("click", async () => {
+        Search(ID, inputQ.value)
+    });
 }
 
-function makeLines(data) {
+function showData(data) {
     wrapper.innerHTML = ""
 
     data.forEach(lineData => {
         makeLine(lineData)
-        getElById(makeButtonId(lineData["id"])).addEventListener("click", async () => {
-            let urls = []
-            getElsByName(makeDetailsId(lineData["id"])).forEach(a => urls.push(a.value))
-            Search(urls, inputQ.value)
-        });
+
+        // link search button to Search
+        // getElById(makeButtonId(lineData["id"])).addEventListener("click", async () => {
+        //     let urls = []
+        //     getElsByName(makeDetailsId(lineData["id"])).forEach(a => urls.push(a.value))
+        //     Search(urls, inputQ.value)
+        // });
+
+        // summary to input
+        getElById(makeDetailsId(lineData["id"])).addEventListener("toggle", (e) => {
+            summary = getElById(makeSummaryId(lineData["id"]))
+            if (e.target.open) {
+                const inputOfSummary = document.createElement("input")
+                inputOfSummary.value = summary.textContent
+                summary.textContent = ""
+                summary.appendChild(inputOfSummary)
+            } else {
+                const inputOfSummary = summary.getElementsByTagName("input")[0]
+                summary.textContent = inputOfSummary.value
+                inputOfSummary.remove()
+            }
+        })
     })
 
     // add input form to append group
@@ -104,10 +137,12 @@ function makeLines(data) {
     input.addEventListener("keydown", (e) => {
         if (e.key === "Enter" || e.key === "Tab") {
             if (input.value !== "") {
-                addSummary(data, input.value)
+                addSummary(input.value)
             }
         }
     })
+
+
 }
 
 
@@ -134,8 +169,12 @@ function getElsByClass(className) {
     return document.getElementsByClassName(className)
 }
 
-function Search(urls, inpv) {
-    urls.forEach(url => {
-        window.open(url + inpv, "_blank")
+function Search(ID, inpv) {
+    getDataAndDo((details) => {
+        details.filter(d => d["id"] === ID).forEach((detail) => {
+            detail["urls"].forEach(url => {
+                window.open(url + inpv, "_blank")
+            })
+        })
     })
 }
