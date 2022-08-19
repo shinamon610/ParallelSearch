2a563dc53979bd837a33b2344878cdaba0c7b294
shinamon <76248816+shinamon610@users.noreply.github.com>
2022-08-19 15:47:30 +0900

    implement search function

--- popup.js
+++ popup.js
@@ -1,8 +1,19 @@
-document.getElementById("btn").addEventListener("click", async () => {
-    onRun(document.getElementById("inp").value)
+document.getElementById("btn0").addEventListener("click", async () => {
+    let urls = []
+    getElByName("dt0").forEach(a => urls.push(a.value))
+    onRun(urls, getElById("inp").value)
 });
 
-function onRun(inp) {
-    console.log(inp)
-    window.open("https://stackoverflow.com/search?q=" + inp, "_blank")
+function getElById(id) {
+    return document.getElementById(id)
+}
+
+function getElByName(name) {
+    return document.getElementsByName(name)
+}
+
+function onRun(urls, inpv) {
+    urls.forEach(url => {
+        window.open(url + inpv, "_blank")
+    })
 }
